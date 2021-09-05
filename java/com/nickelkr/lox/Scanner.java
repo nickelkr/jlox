6ea46fdaba9d68d28a8eaf7c50e243d6b44a83e8
Kyle Nickel <nickelkr@protonmail.com>
2021-09-04 22:07:42 -0400

    scanTokens

--- java/com/nickelkr/lox/Scanner.java
+++ java/com/nickelkr/lox/Scanner.java
@@ -10,8 +10,22 @@ import static com.nickelkr.lox.TokenType.*;
 class Scanner {
   private final String source;
   private final List<Token> tokens = new ArrayList<>();
+  private int start = 0;
+  private int current = 0;
+  private int line = 1;
 
   Scanner(String source) {
     this.source = source;
+  }
+
+  List<Token> scanTokens() {
+    while (!isAtEnd()) {
+      // We are at the beginning of the next lexeme
+      start = current;
+      scanToken();
+    }
+
+    tokens.add(new Token(EOF, "", null, line));
+    return tokens;
   }
 }
