5e3516163010f42d331f0e03c6774e01b969ff54
Kyle Nickel <nickelkr@protonmail.com>
2021-09-04 21:55:38 -0400

    Report when errors bubble

--- java/com/nickelkr/lox/Lox.java
+++ java/com/nickelkr/lox/Lox.java
@@ -9,6 +9,8 @@ import java.nio.file.Paths;
 import java.util.List;
 
 public class Lox {
+  static boolean hadError = false;
+
   public static void main(String[] args) throws IOException {
     if (args.length > 1) {
       System.out.println("Usage: jlox [script]");
@@ -24,6 +26,8 @@ public class Lox {
 private static void runFile(String path) throws IOException {
   byte[] bytes = Files.readAllBytes(Paths.get(path));
   run(new String(bytes, Charset.defaultCharset()));
+  // Indicate an error in the exit code
+  if (hadError) System.exit(65);
 }
 
 private static void runPrompt() throws IOException {
@@ -35,6 +39,7 @@ private static void runPrompt() throws IOException {
     String line = reader.readLine();
     if (line == null) break;
     run(line);
+    hadError = false;
   }
 }
 
