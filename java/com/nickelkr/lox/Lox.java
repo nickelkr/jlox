0917569be58a67bea96b0a95b9e97241c1d3ab59
Kyle Nickel <nickelkr@protonmail.com>
2021-09-04 21:49:46 -0400

    Stubbed entry points

--- java/com/nickelkr/lox/Lox.java
+++ java/com/nickelkr/lox/Lox.java
@@ -12,10 +12,38 @@ public class Lox {
   public static void main(String[] args) throws IOException {
     if (args.length > 1) {
       System.out.println("Usage: jlox [script]");
+      System.exit(64);
     } else if (args.length == 1) {
-      System.out.println("runfile");
+      runFile(args[0]);
     } else {
-      System.out.println("run prompt");
+      runPrompt();
     }
+  }
+}
+
+private static void runFile(String path) throws IOException {
+  byte[] bytes = Files.readAllBytes(Paths.get(path));
+  run(new String(bytes, Charset.defaultCharset()));
+}
+
+private static void runPrompt() throws IOException {
+  InputStreamReader input = new InputStreamReader(System.in);
+  BufferedReader reader = new BufferedReader(input);
+
+  for (;;) {
+    System.out.rint("> ");
+    String line = reader.readLine();
+    if (line == null) break;
+    run(line);
+  }
+}
+
+private static void run(String source) {
+  Scanner scanner = new Scanner(source);
+  List<Token> tokens = scanner.scanTokens();
+
+  // For now, just print the tokens.
+  for (Token token : tokens) {
+    System.out.println(token);
   }
 }
