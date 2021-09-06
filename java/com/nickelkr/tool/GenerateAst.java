10d70cd28854319496adf31a8d2b356fd8cba0f7
Kyle Nickel <nickelkr@protonmail.com>
2021-09-05 22:00:11 -0400

    Build expression super class

--- java/com/nickelkr/tool/GenerateAst.java
+++ java/com/nickelkr/tool/GenerateAst.java
@@ -12,5 +12,24 @@ public class GenerateAst {
       System.exit.(64);
     }
     String outputDir = args[0];
+    defineAst(outputDir, "Expr", Arrays.asList(
+          "Binary : Expr left, Token operator, Expr right",
+          "Grouping : Expr expression",
+          "Literal : Object value",
+          "Unary : Token operator, Expr right"
+    ));
+  }
+
+  private static void defineAst(String outputDir, String baseName, List<String> types) throws IOException {
+    String path = outputDir + "/" + baseName + ".java";
+    PrintWriter writer = new PrintWriter(path, "UTF-8");
+
+    writer.println("package com.nickelkr.lox;");
+    writer.println();
+    writer.println("import java.util.List;");
+    writer.println();
+    writer.println("abstract class " + baseName + " {");
+    writer.println("}");
+    writer.close();
   }
 }
