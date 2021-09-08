dd7503baca1f86f465ded4b796488e3818f326aa
Kyle Nickel <nickelkr@protonmail.com>
2021-09-07 22:14:36 -0400

    Initial Parser class

---
+++ java/com/nickelkr/lox/Parser.java
@@ -0,0 +1,14 @@
+package com.nickelkr.lox;
+
+import java.util.List;
+
+import static com.nickelkr.lox.TokenType.*;
+
+class Parser {
+  private final List<Token> tokens;
+  private int current = 0;
+
+  Parser(List<Token> tokens) {
+    this.tokens = tokens;
+  }
+}
